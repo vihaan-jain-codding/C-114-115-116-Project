e424e4329f8b94dc171703854ddf2bac7cfa4442
vihaan-jain-codding <94980425+vihaan-jain-codding@users.noreply.github.com>
2023-11-04 15:38:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload(){
-
+mustacheImage = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -19,13 +21,16 @@ function modelloaded(){
 function gotposes(results){
     if (results.length > 0){
         console.log(results);
-        console.log("Nose X = " + results[0].pose.nose.x);
-        console.log("Nose Y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        console.log("Nose X = " + noseX);
+        noseY = results[0].pose.nose.y;
+        console.log("Nose Y = " + noseY);
     }
 }
 
 function draw(){
  image(video, 0, 0, 300, 300);
+ image(mustacheImage, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
